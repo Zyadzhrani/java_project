88420999e7e94a549e356e545f53101a167d332d
Zyadzhrani <123445553+Zyadzhrani@users.noreply.github.com>
2023-11-26 17:36:07 +0300

    ..

--- src/main/java/phase1/Phase1.java
+++ src/main/java/phase1/Phase1.java
@@ -1,6 +1,7 @@
 package phase1;
 
 import Classes.Elderly;
+import javafx.animation.Interpolator;
 import javafx.animation.PathTransition;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -12,6 +13,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
@@ -25,12 +27,32 @@ public class Phase1 extends Application {
     public void start(Stage stage) throws InterruptedException, IOException {
 
         // Create the root container (AnchorPane)
-        Parent root = new FXMLLoader().load(getClass().getResource("/PHASE.fxml"));
+        Parent fxmlRoot = new FXMLLoader().load(getClass().getResource("/PHASE.fxml"));
         // Create the controller and move the circles
         Phase1Controller controller = new Phase1Controller();
         Elderly E1 = new Elderly("A",11,null);
         Circle c12 = controller.make_circle(E1);
+        Group root = new Group(); //Creating a Group
+        root.getChildren().add(fxmlRoot);
 
+
+
+
+
+        root.getChildren().add(c12);
+
+        // Create a path transition
+        Line straightLine = (Line) root.lookup("#Line1");
+        Line line = new Line(-100,0,-100,500);
+        PathTransition pathTransition = new PathTransition();
+        pathTransition.setNode(c12);
+        pathTransition.setPath(straightLine);
+        pathTransition.setInterpolator(Interpolator.LINEAR);
+        pathTransition.setCycleCount(PathTransition.INDEFINITE); // Repeat indefinitely
+        pathTransition.setDuration(Duration.seconds(1)); // Duration for one cycle
+
+        // Start the animation
+        pathTransition.play();
 
 
 
@@ -38,7 +60,7 @@ public class Phase1 extends Application {
         // Create a scene and set it to the stage
         Scene scene = new Scene(root);
         stage.setTitle("Team 1");
-        stage.setResizable(false); // Display the Resizable
+        stage.setResizable(true); // Display the Resizable
         stage.setScene(scene);
         stage.show();
     }
